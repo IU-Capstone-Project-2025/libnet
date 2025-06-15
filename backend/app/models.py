593f97cdfdfd1b7993321b4748cc459e07067326
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-06-15 18:21:06 +0300

    Add CRUD for users and books

---
+++ backend/app/models.py
@@ -0,0 +1,57 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from app.database import engine
+from datetime import date
+
+
+class LibUser(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, index=True)
+    first_name: str
+    last_name: str
+    email: str = Field(index=True, unique=True)
+    phone: str = Field(default=None)
+    city: str = Field(default=None)
+    role: str = Field(default="user")
+
+class LibUserUpdate(SQLModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    city: Optional[str] = None
+    role: Optional[str] = None
+
+class Library(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    city: str
+    address: str
+
+class Book(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title: str
+    author: str
+    description: str
+    year: int
+
+class BookUpdate(SQLModel):
+    id: Optional[int] = None
+    title: Optional[str] = None
+    author: Optional[str] = None
+    description: Optional[str] = None
+    year: Optional[int] = None
+
+class LibraryBook(SQLModel, table=True):
+    library_id: int = Field(foreign_key="library.id", primary_key=True)
+    book_id: int = Field(foreign_key="book.id", primary_key=True)
+    quantity: int
+
+class Booking(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="libuser.id")
+    book_id: int = Field(foreign_key="book.id")
+    library_id: int = Field(foreign_key="library.id")
+    date_from: date
+    date_to: date
+
+SQLModel.metadata.create_all(engine)
---
+++ backend/app/routers/users.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from app.database import get_session
+from app import models
+import re
+
+router = APIRouter()
+
+# Create a User
+@router.post("/", response_model=models.LibUser)
+def create_user(user: models.LibUser, db: Session = Depends(get_session)):
+    existing_user = db.exec(select(models.LibUser).where(models.LibUser.email == user.email)).first()
+    roles = ["user", "manager", "admin"]
+    email_pattern = r'^[^@]+@[^@]+\.[^@]+$'
+    data = [user.first_name, user.last_name, user.email, user.phone, user.city]
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="User with this email already exists")
+    if user.role not in roles:
+        raise HTTPException(status_code=400, detail="User role is invalid")
+    if None in data:
+        raise HTTPException(status_code=400, detail="Some fields are missing")
+    if not bool(re.match(email_pattern, user.email)):
+        raise HTTPException(status_code=400, detail="Email address is invalid")
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    print(user.id)
+    return user
+
+# Get users
+@router.get("/", response_model=list[models.LibUser])
+def read_users(db: Session = Depends(get_session)):
+    users = db.exec(select(models.LibUser)).all()
+    return users
+
+# Get single user
+@router.get("/{_id}", response_model=list[models.LibUser])
+def read_users(_id: int, db: Session = Depends(get_session)):
+    user = db.exec(select(models.LibUser).where(models.LibUser.id == _id)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+# Update user
+@router.patch("/users/{_id}")
+def update_user(_id: int, user_update: models.LibUserUpdate, session: Session = Depends(get_session)):
+    user = session.exec(select(models.LibUser).where(models.LibUser.id == _id)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User does not exist")
+
+    user_data = user_update.dict(exclude_unset=True)
+    for key, value in user_data.items():
+        setattr(user, key, value)
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+@router.delete("/{_id}")
+def delete_user(_id: int, db: Session = Depends(get_session)):
+    user = db.exec(select(models.LibUser).where(models.LibUser.id == _id)).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User does not exist")
+    db.delete(user)
+    db.commit()
+
