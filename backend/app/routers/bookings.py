a12054f09b99bcf6f4f55a0048b54241c4f340e8
flowelx <apple.pie.x@inbox.ru>
2025-06-16 20:31:45 +0300

    Add CRUD for bookings

--- backend/app/routers/bookings.py
+++ backend/app/routers/bookings.py
@@ -1,3 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from app.database import get_session
+from app import models
 
 router = APIRouter()
+
+# Create a Booking
+@router.post("/", response_model=models.Booking)
+def create_booking(booking: models.Booking, db: Session = Depends(get_session)):
+    db.add(booking)
+    db.commit()
+    db.refresh(booking)
+    return booking
+
+# Get all Bookings
+@router.get("/", response_model=list[models.Booking])
+def get_bookings(db: Session = Depends(get_session)):
+    bookings = db.exec(select(models.Booking)).all()
+    return bookings
+
+# Get single Booking
+@router.get("/{booking_id}", response_model=models.Booking)
+def get_booking(booking_id: int, db: Session = Depends(get_session)):
+    booking = db.exec(select(models.Booking).where(models.Booking.id == booking_id)).first()
+    if not booking:
+        raise HTTPException(status_code = 404, detail="Bookinf not found")
+    return booking
+
+# Delete a Booking
+@router.delete("/{booking_id}", response_model=models.Booking)
+def update_bookings(booking_id: int, db: Session = Depends(get_session)):
+    booking = db.exec(select(models.Booking).where(models.Booking.id == booking_id)).first()
+    if not booking:
+        raise HTTPException(status_code = 404, detail="Booking not found")
+
+    db.delete(booking)
+    db.commit()
