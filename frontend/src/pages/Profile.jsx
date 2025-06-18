8fe85fb410b43c5e9f395bb76e9db8b9872c0ffc
thisismykingddomcum@gmail.com <kvassoedik@gmail.com>
2025-06-18 16:48:21 +0300

    profile fields

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,62 @@
-import React from 'react';
+import {React, useState} from 'react';
+import { useAuth } from '../context/AuthContext';
 
 export default function Profile() {
-  return <h1>Профиль</h1>;
+  const { user } = useAuth();
+
+  const [email, setEmail] = useState(user.email);
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [phone, setPhone] = useState(user.phone);
+  const [city, setCity] = useState(user.setCity);
+
+  return (
+    <>
+      <label for='user__city'>Выберите город:</label>
+      <select className='user__city'>
+        <option value="1">Иннополис</option>
+        <option value="1">Казань</option>
+      </select><br/>
+
+      <label for='user__firstName'>Ваше имя</label>
+      <input
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      /><br/>
+
+      <label for='user__lastName'>Ваша фамилия</label>
+      <input
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      /><br/>
+
+      <label for='user__phone'>Номер телефона</label>
+      <input
+        value={phone}
+        onChange={e => setPhone(e.target.value)}
+      /><br/>
+
+      <label for='user__email'>E-mail</label>
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      /><br/>
+
+      <label for='user__password'>Пароль</label>
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      /><br/>
+
+      {/* <label for='user__lastName'>Дата род</label>
+      <input
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      /> */}
+
+      <button>Сохранить</button>
+      <button>Выйти из аккаунта</button>
+    </>
+  )
 }
