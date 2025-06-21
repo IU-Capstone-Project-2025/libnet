2de69fb7e5cbe3977a52bec5ba8d1fa29f7e87e8
artisaaep <104840798+artisaaep@users.noreply.github.com>
2025-06-21 15:45:18 +0300

    Add books quantity functional

--- backend/app/routers/books.py
+++ backend/app/routers/books.py
@@ -6,16 +6,28 @@ from app import models
 router = APIRouter()
 
 # Create a Book
-@router.post("/", response_model=models.Book)
-def create_book(book: models.Book, db: Session = Depends(get_session)):
+@router.post("/{quantity}", response_model=models.Book)
+def create_book(book: models.Book, quantity: int, db: Session = Depends(get_session)):
     db.add(book)
     db.commit()
     db.refresh(book)
+    library_book = models.LibraryBook(library_id=book.library_id, book_id=book.id, quantity=quantity)
+    db.add(library_book)
+    db.commit()
+    db.refresh(library_book)
     return book
+
+# Get quantity of books in a library
+@router.get("/quantity/{library_id}/{book_id}", response_model=int)
+def get_book_quantity(library_id: int, book_id: int, db: Session = Depends(get_session)):
+    book = db.exec(select(models.LibraryBook).where(models.LibraryBook.library_id == library_id and models.LibraryBook.book_id == book_id)).first()
+    if not book:
+        raise HTTPException(status_code=404, detail="Book does not exist in this library")
+    return book.quantity
 
 # Get all Books
 @router.get("/", response_model=list[models.Book])
-def read_books(db: Session = Depends(get_session)):
+def get_all_books(db: Session = Depends(get_session)):
     books = db.exec(select(models.Book)).all()
     return books
 
@@ -54,7 +66,7 @@ def update_book(book_id: int, book_update: models.BookUpdate, db: Session = Depe
     return book
 
 # Delete a Book
-@router.delete("/{book_id}")
+@router.delete("/{book_id}", status_code=204)
 def delete_book(book_id: int, db: Session = Depends(get_session)):
     book = db.exec(select(models.Book).where(models.Book.id == book_id)).first()
     if not book:
