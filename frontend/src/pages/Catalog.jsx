518f1f3fbf26c29993b16ca3f2c1093d7bf09be7
Ivan Murzin <vanya.murzin@inbox.ru>
2025-06-22 16:42:47 +0300

    Fix sidebar styles and add styles for hover and active

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './Catalog.css';
 
 export default function Catalog() {
@@ -30,16 +30,39 @@ export default function Catalog() {
   if (loading) return <p>Загружаем…</p>;
   if (error) return <p style={{ color: 'red' }}>Ошибка: {error}</p>;
 
-
-
   return (
     <>
       <h1 className="user__heading">Каталог книг</h1>
       <div className="user__genre-section">
         <div className="user__sidebar">
-          <h2>Жанры</h2>
-          <h3>Триллер</h3>
-          <h3>Приключения</h3>
+          <h2 className="user__sidebar-heading">Жанры</h2>
+          <ul className="user__genre-list">
+            <li className="user__genre-item">
+              <Link to="/" className="user__genre-link">
+                Приключения
+              </Link>
+            </li>
+            <li className="user__genre-item">
+              <Link to="/" className="user__genre-link">
+                Детектив
+              </Link>
+            </li>
+            <li className="user__genre-item">
+              <Link to="" className="user__genre-link">
+                Исторический роман
+              </Link>
+            </li>
+            <li className="user__genre-item ">
+              <Link to="" className="user__genre-link user__genre-item--active">
+                Фантастика
+              </Link>
+            </li>
+            <li className="user__genre-item">
+              <Link to="" className="user__genre-link">
+                Фэнтези
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="user__books-list">
           {books.map((b) => (
@@ -59,7 +82,14 @@ export default function Catalog() {
                   <span className="user__book-info-author">{b.author}</span>
                 </div>
                 <div className="user__book-like-button">
-                  <img className="user__book-like-icon" src='/like-2.svg' alt="Лайк" width={"40px"} height={"40px"}></img> {/*  TODO: change image, don't set to ""*/}
+                  <img
+                    className="user__book-like-icon"
+                    src="/like-2.svg"
+                    alt="Лайк"
+                    width={'40px'}
+                    height={'40px'}
+                  ></img>{' '}
+                  {/*  TODO: change image, don't set to ""*/}
                 </div>
               </div>
             </div>
