9af2a57e2478ea2d03f1f65c893cb249e14648d7
kvassoedik <kvassoedik@gmail.com>
2025-06-25 11:52:31 +0300

    fix get book by isbn

--- backend/app/routers/libraries.py
+++ backend/app/routers/libraries.py
@@ -95,8 +95,8 @@ def delete_library(library_id: int, db: Session = Depends(get_session)):
 @router.get("/isbn/{library_id}/{book_isbn}", response_model=models.Book)
 def get_book_isbn(library_id: int, book_isbn: str, db: Session = Depends(get_session)):
     book = db.exec(select(models.Book).where(and_(
-            models.Book.isbn == book_isbn,
-            models.Book.library_id == library_id
+            models.Book.library_id == library_id,
+            models.Book.isbn == book_isbn
         ))).first()
     if not book:
         raise HTTPException(status_code=404, detail="Book does not exist")
